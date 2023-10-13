351577ef1e75496e66469a1758c3da8d8bf6cc74
Pedro Diaz Jr. <Diaz850@gmail.com>
2023-10-13 18:44:52 -0500

    created dynamic js

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-function getName(){let userName = prompt('What is your name?');
-return userName
-}
-getName();
+'use strict';
 
-function greetUser(name){
-document.write('Welcome' + name + '!')
+function getAge() {
+    let userName = prompt('What is your age?');
+    let ageint = parseInt(userName) 
+if (ageint >= 18){
+    document.write( ageint + ' looks good on you!')
+}
+else{
+    alert('Come back when youre old enough');
+}
+}
+getAge();
+
+
+
+function greetUser() {
+    console.log('Welcome to the console page!')
+}
+greetUser()
+
+for(let i= 3;i <10; i++){
+    console.log(i)}
+
+for(let i = 10; i >=0; i--){
+    console.log('you have' + i + 'turns left')
 }
